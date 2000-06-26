1b322fc3554663f58edb2e2a1a8744dbb245e58c
rw7 <rw7@localhost>
2000-06-26 17:21:11 +0000

    *** empty log message ***

--- ocl/src/regress.sh
+++ ocl/src/regress.sh
@@ -1,11 +1,8 @@
-#!/usr/local/bin/tcsh
-
-rm regress.results
 
 java \
   -classpath .:xml4j.jar:test.jar \
   -Dtudresden.ocl.check.types.xmifacade.log=yes \
-  test.ui.TestRunner tudresden.ocl.test.TestAll >> regress.results
+  test.ui.TestRunner tudresden.ocl.test.TestAll > regress.results
 
 echo --- diffs in regress.results
 diff regress.results regress.results.reference
