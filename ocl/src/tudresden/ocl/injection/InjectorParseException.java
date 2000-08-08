b66540ebd648b5f1d7607b000e2e8434e892fb3c
rw7 <rw7@localhost>
2000-08-08 12:41:06 +0000

    javadoc fix

--- ocl/src/tudresden/ocl/injection/InjectorParseException.java
+++ ocl/src/tudresden/ocl/injection/InjectorParseException.java
@@ -22,7 +22,7 @@ package tudresden.ocl.injection;
    Thrown by the java parser, if the input stream is not valid 
    java language. Should never be thrown on java code, which passes
    javac sucessfully (otherwise it's a bug.)
-   @Injector
+   @see Injector
 */
 public class InjectorParseException extends Exception
 {
