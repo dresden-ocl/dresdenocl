b458bd62479a8170702a85f5687131ccf3face69
rw7 <rw7@localhost>
2001-08-19 16:11:50 +0000

    first try for java constraints

---
+++ ocl/src/tudresden/ocl/injection/test/ContractImplementation.java
@@ -0,0 +1,87 @@
+package tudresden.ocl.injection.test;
+
+
+class ContractImplementation
+{
+	private void assert(boolean condition)
+	{
+	}
+	
+	private void assert(boolean condition, String message)
+	{
+	}
+	
+	private int state;
+	
+	/**
+	 * @assumethrows IllegalArgumentException state!=0
+	 * @assumethrows ClassCastException x instanceof Comparable
+	 * @assumethrows ClassCastException y instanceof Comparable
+	 * @assume x!=y
+	 *
+	 * @ensurethrows RuntimeException state==0
+	 * @ensurethrows NullPointerException x==null
+	 * @ensurethrows NullPointerException y==null
+	 * @ensure result>=0
+	 */
+	public int compare_wrapped_tudresden_ocl_injection_test_ContractImplementation(Object x, Object y)
+	{
+		if(state==0)
+			throw new IllegalArgumentException();
+		Comparable cx = (Comparable) x;
+		Comparable cy = (Comparable) y;
+		if(x==y)
+			throw new RuntimeException();
+		System.out.println(x.toString());
+		System.out.println(y.toString());
+		state = 0;
+		return 0;
+	}
+	
+	public int compare(Object x, Object y)
+	{
+		boolean
+		assumeThrowAny=true,
+		assumeThrow0=false,
+		assumeThrow1=false,
+		assumeThrow2=false,
+		ensureThrowAny=false,
+		ensureThrow0=false,
+		ensureThrow1=false,
+		ensureThrow2=false;
+		
+		if(!(state!=0)) assumeThrow0=true;
+		else if(!(x instanceof Comparable)) assumeThrow1=true;
+		else if(!(x instanceof Comparable)) assumeThrow2=true;
+		else
+		{
+			assumeThrowAny=false;
+			ensureThrowAny=true;
+			if(state==0) ensureThrow0=true;
+			else if(x==null) ensureThrow1=true;
+			else if(y==null) ensureThrow2=true;
+			else ensureThrowAny=false;
+		}
+		
+		int result;
+		try
+		{
+			result=compare_wrapped_tudresden_ocl_injection_test_ContractImplementation(x, y);
+			assert(ensureThrow0, "should have thrown IllegalArgumentException, since state=0");
+			assert(ensureThrow1, "should have thrown NullPointerException, since x==null");
+			assert(ensureThrow2, "should have thrown NullPointerException, since y==null");
+			assert(result>=0, "ensure violated: result>=0");
+		}
+		catch(ClassCastException e)
+		{
+			assert(assumeThrow1||assumeThrow2, "should not");
+			throw e;
+		}
+		return result;
+	}
+	
+}
+
+
+
+
