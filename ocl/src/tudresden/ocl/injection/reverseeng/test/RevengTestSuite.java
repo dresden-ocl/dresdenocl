eb4fb2756ffdc011b2cdc8bd219e3c6b420e198a
rw7 <rw7@localhost>
2001-09-01 11:51:33 +0000

    modified the test to use Diff.diff, this should make the test working with windows line breaks.

--- ocl/src/tudresden/ocl/injection/reverseeng/test/RevengTestSuite.java
+++ ocl/src/tudresden/ocl/injection/reverseeng/test/RevengTestSuite.java
@@ -32,6 +32,7 @@ import java.util.*;
 
 import tudresden.ocl.injection.*;
 import tudresden.ocl.injection.reverseeng.*;
+import tudresden.ocl.test.*;
 
 /** 
   * Testsuite testing the vital parts of the reverseeng stuff.
@@ -41,69 +42,24 @@ import tudresden.ocl.injection.reverseeng.*;
   */
 public class RevengTestSuite extends TestCase {
 
-  private static class WrappedException extends Error {
-    
-    protected Throwable m_tWrapped;
-    
-    public WrappedException (Throwable tWrapped) {
-      super();
-      
-      m_tWrapped = tWrapped;
-    }
-    
-    public String getMessage() {
-      return m_tWrapped.getMessage();
-    }
-    
-    public String getLocalizedMessage() {
-      return m_tWrapped.getLocalizedMessage();
-    }
-    
-    public String toString() {
-      return m_tWrapped.toString();
-    }
-    
-    public void printStackTrace() {
-      m_tWrapped.printStackTrace();
-    }
-    
-    public void printStackTrace (PrintStream ps) {
-      m_tWrapped.printStackTrace (ps);
-    }
-    
-    public void printStackTrace (PrintWriter pw) {
-      m_tWrapped.printStackTrace (pw);
-    }
-    
-    public Throwable fillInStackTrace() {
-      // No fill in, as we want to maintain the original stack trace
-      return this;
-    }
-  }
-  
   protected InputStream m_isSource;
   protected Reader m_rSource;
 
   protected InputStream m_isSource2;
   protected Reader m_rSource2;
   
-  protected Reader m_rModificationTemplate;
-  
   /** Creates new RevengTestSuite */
   public RevengTestSuite (String sName) {
     super (sName);
   }
   
-  protected void setUp() {
+  protected void setUp() throws IOException {
     // Setup files to be handled by the tests
     m_isSource = getClass().getResourceAsStream ("ShortExample.java");
     m_rSource = new BufferedReader (new InputStreamReader (m_isSource));
 
     m_isSource2 = getClass().getResourceAsStream ("ShortExample.java");
     m_rSource2 = new BufferedReader (new InputStreamReader (m_isSource2));
-    
-    // For comparison in testSave
-    m_rModificationTemplate = new BufferedReader (new InputStreamReader (getClass().getResourceAsStream ("ShortExample.testmodified")));
     
   }
   
@@ -118,31 +74,21 @@ public class RevengTestSuite extends TestCase {
     catch (IOException ioe) {}
   }
 
-  public void testAnalysis() {
+  public void testAnalysis() throws Exception {
     AnalysisConsumer ac = new AnalysisConsumer ("ShortExample.java");
     
-    try {
-      new Injector (m_rSource, null, ac).parseFile();
-    }
-    catch (Throwable t) {
-      throw new WrappedException (t);
-    }
+    new Injector (m_rSource, null, ac).parseFile();
     
     String sCompare = "[Collection<null> m_llCollectionTester at comment ID 2, Collection<null> m_alCollectionTesterNoComment at comment ID 3, Map<null -> null> m_mMapTester at comment ID 4, Map<null -> null> m_hmMapTesterNoComment at comment ID 5, Collection<null> m_sBag at comment ID 6]";
     
     assertEquals (ac.getAllFeatures().toString(), sCompare);
   }
   
-  public void testSaving() {
+  public void testSaving() throws Exception {
     // First read in file
     AnalysisConsumer ac = new AnalysisConsumer ("ShortExample.java");
     
-    try {
-      new Injector (m_rSource, null, ac).parseFile();
-    }
-    catch (Throwable t) {
-      throw new WrappedException (t);
-    }
+    new Injector (m_rSource, null, ac).parseFile();
     
     // Now manipulate one feature
     List lFeatures = ac.getAllFeatures();
@@ -154,68 +100,14 @@ public class RevengTestSuite extends TestCase {
     cdNoComments.setElementType ("NoCommentCollectionElementType");
     
     // Now test saving...
-    try {
-      
-      // Compare stream written to stream previously written...
-      class ComparisonWriter extends Writer {
-        int m_nLine = 1;
-        int m_nColumn = 1;
-        
-        public void write (char[] cbuf, int off, int len) {
-          try {
-            // Compare and conditionally throw exception
-            for (int i = 0; i < len; i++) {
-              int nChar = m_rModificationTemplate.read();
-              assertTrue ("Attempt to write wrong character.\nGot \'" + cbuf[off + i] + "\' expected \'" + (char)nChar + "\' at " +
-                      "(" + m_nLine + ", " + m_nColumn + ").",
-                       (cbuf[off + i] == nChar));
-              
-              if (cbuf[off + i] != '\n') {
-                m_nColumn ++;
-              }
-              else {
-                m_nColumn = 1;
-                m_nLine++;
-              }
-            }
-          }
-          catch (AssertionFailedError afe) {
-            throw afe;
-          }
-          catch (Throwable t) {
-            throw new WrappedException (t);
-          }
-        }
-        
-        public void flush() {}
-        
-        public void close() {
-          try {
-            assertTrue ("Premature end of stream!",
-                     m_rModificationTemplate.read() == -1);
-          }
-          catch (AssertionFailedError afe) {
-            throw afe;
-          }
-          catch (Throwable t) {
-            throw new WrappedException (t);
-          }
-        }
-      }
-      
-      IndentAwareWriter iaw = new IndentAwareWriter (new ComparisonWriter());
+    IndentAwareWriter iaw = new IndentAwareWriter (new FileWriter("ShortExample.java.bak"));
+    new Injector (m_rSource2, iaw, new FileSaveConsumer (iaw, ac)).parseFile();
+    iaw.close();
+    iaw = null;
+			
+    // For comparison in testSave
+		Diff.diff(new DiffSource(getClass().getResource("ShortExample.testmodified")), new DiffSource(new File("ShortExample.java.bak")));
     
-      new Injector (m_rSource2, iaw, new FileSaveConsumer (iaw, ac)).parseFile();
-      
-      iaw.close();
-      iaw = null;
-    }
-    catch (AssertionFailedError afe) {
-      throw afe;
-    }
-    catch (Throwable t) {
-      throw new WrappedException (t);
-    }
   }
   
   public static Test suite() {
