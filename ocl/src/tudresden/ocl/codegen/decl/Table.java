1815abec066b1d668f1efb4f6cecded5b3c76c3e
ak19 <ak19@localhost>
2002-02-27 15:31:24 +0000

    added real FK-Table references

--- ocl/src/tudresden/ocl/codegen/decl/Table.java
+++ ocl/src/tudresden/ocl/codegen/decl/Table.java
@@ -35,11 +35,17 @@ import java.util.*;
  * them to the specific database system types if necessary. The foreign
  * keys are ordinary columns that are associated to additional information.
  * These information are:<br>
- * - the name of a foreign table;<br>
+ * - the name of a foreign table;(deprecated)<br>
+ * modified for reference to foreign table object <BR>
  * - the name of a column in the foreign table.<br>
+ * - possibly other columns that are part of the same (multiple) foreign key
  * @author Sten Loecher
- * @version 2.0 (Entirely new implementation due to new requirements.)
+ *
+ * @version 2.1 (Revised by Andrea Kling 2002, enhanced foreign key references)
  */
+
+//02/2002 [eja] enhanced FK-references
+//              added optional-flag for columns
 public class Table {
 
 	private static String EX_DUPL_COL = "Column still exists: ";
@@ -133,15 +139,17 @@ public class Table {
 		} else {
 			pkCol.pk = true;
 		}
-	}
+  }
 
 	/**
 	 * Sets the specified column to foreign key state.
 	 * @param colName the column name
 	 * @param fkTable the name of the foreign table
-	 * @param pkTable the name of the foreign column
+   * @param fkColumn the name of the foreign column
 	 * @exception NullPointerException if one of colName, fkTable or fkColumn is null
          * @exception IllegalArgumentException if colName does not exists
+   *
+   * @deprecated use setForeignKey(String colName, Table fkTable, String fkColumn) instead
 	 */
 	public void setForeignKey(String colName, String fkTable, String fkColumn)
 	throws NullPointerException, IllegalArgumentException {
@@ -157,6 +165,80 @@ public class Table {
 			fkCol.fkTable = fkTable;
 			fkCol.fkColumn = fkColumn;
 		}
+	}
+  /**
+	 * Sets the specified column to foreign key state.
+	 * @param colName the column name
+   * @param fkTable the foreign table
+   * @param fkColumn the name of the foreign column
+	 * @exception NullPointerException if one of colName, fkTable or fkColumn is null
+         * @exception IllegalArgumentException if colName does not exists
+	 */
+  public void setForeignKey(String colName, Table fkTable, String fkColumn)
+	throws NullPointerException, IllegalArgumentException {
+		if ((colName == null) || (fkTable == null) || (fkColumn == null))
+			throw new NullPointerException(EX_FK_SNOTS);
+    if(!fkTable.isColumn(fkColumn))
+      throw new IllegalArgumentException(EX_COL_NE + fkColumn);
+		Column fkCol = getColumn(colName);
+
+		if (fkCol == null) {
+			throw new IllegalArgumentException(EX_COL_NE + colName);
+		} else {
+      if(! getColumnType(colName).equals(fkTable.getColumnType(fkColumn)))
+        throw new IllegalArgumentException("Column types do not match: "+
+        getTableName()+": "+colName+", "+fkTable.getTableName()+": "+
+        fkColumn);
+			fkCol.fk = true;
+      fkCol.realFKTable = fkTable;
+      fkCol.fkTable = fkTable.getTableName();
+			fkCol.fkColumn = fkColumn;
+      fkTable.addReferringTable(fkColumn, this, colName);
+      String fkType = fkTable.getColumnType(fkColumn).trim().toLowerCase();
+      if(fkType.equals("id") || fkType.equals("oid"))
+        fkCol.type = "long";
+		}
+	}
+
+  /**
+   * Marks the specified columns as multi column foreign key reference.
+   * @param colNames the column names that are part of the foreign key
+   * @param fkTable the foreign table
+   * @param fkColumns the names of the corresponding foreign columns
+	 * @exception NullPointerException if one of colName, fkTable or fkColumn is null
+   * @exception IllegalArgumentException if colName does not exists or columns do not match foreign columns
+	 */
+
+  public void setForeignKey(String[] colNames, Table fkTable, String[] fkColumns)
+	throws NullPointerException, IllegalArgumentException {
+    if ((colNames == null) || (fkTable == null) || (fkColumns == null))
+			throw new NullPointerException(EX_FK_SNOTS);
+    if(colNames.length != fkColumns.length)
+      throw new IllegalArgumentException("Keys do not match");
+
+    for(int i=0; i< fkColumns.length; i++){
+
+      if(!fkTable.isColumn(fkColumns[i]))
+        throw new IllegalArgumentException(EX_COL_NE + fkColumns[i]);
+      Column fkCol = getColumn(colNames[i]);
+
+      if (fkCol == null) {
+        throw new IllegalArgumentException(EX_COL_NE + colNames[i]);
+      } else {
+        fkCol.multiColumnFK = colNames;
+        setForeignKey(colNames[i], fkTable, fkColumns[i]);
+      }
+    }
+	}
+
+
+    /**
+     * used to set the reverse table reference for foreign key references
+     */
+  protected void addReferringTable(String referredColumn, Table referringTable, String referringColumn){
+    Column fkCol = getColumn(referredColumn);
+    fkCol.referringTables.add(referringTable);
+    fkCol.referringColumns.add(referringColumn);
 	}
 
 	/**
@@ -261,7 +343,7 @@ public class Table {
 	}
 
 	/**
-	 * @return a String array that contains all foreign key column names
+   * @return a String array that contains all columns referring to another Table
 	 */
 	public String[] getForeignKeyColumns() {
 		List l = new ArrayList();
@@ -296,6 +378,19 @@ public class Table {
 		}
 	}
 
+  public void setColumnType(String colName, String colType)
+	throws NullPointerException, IllegalArgumentException {
+		if (colName == null) throw new NullPointerException(EX_COL_NULL);
+
+		Column c = getColumn(colName);
+
+		if (c == null) {
+			throw new IllegalArgumentException(EX_COL_NE + colName);
+		} else {
+      c.type = colType;
+		}
+	}
+
 	/**
 	 * @param colName the name of a column
 	 * @return the foreign table name of the specified column
@@ -317,6 +412,26 @@ public class Table {
 
 	/**
 	 * @param colName the name of a column
+   * @return the foreign table of the specified column
+	 * @exception NullPointerException if colName is null
+         * @exception IllegalArgumentException if colName does not exists
+	 */
+
+  public Table getReferredTable(String colName)
+	throws NullPointerException, IllegalArgumentException {
+		if (colName == null) throw new NullPointerException(EX_COL_NULL);
+
+		Column c = getColumn(colName);
+
+		if (c == null) {
+			throw new IllegalArgumentException(EX_FCOL_NE + colName);
+		} else {
+      return c.realFKTable;
+		}
+	}
+
+	/**
+	 * @param colName the name of a column
 	 * @return the foreign column name of the specified column
 	 * @exception NullPointerException if colName is null
          * @exception IllegalArgumentException if colName does not exists
@@ -333,26 +448,81 @@ public class Table {
 			return new String(c.fkColumn);
 		}
 	}
-	
+
 	/**
-	 * @return a String representation of all primary key columns, 
+	 * @param colName the name of a column
+   * @return all column names that are part of the same foreign key
+   *         as the specified column (including specified column)
+	 * @exception NullPointerException if colName is null
+         * @exception IllegalArgumentException if colName does not exists
+	 */
+  public String[] getAllForeignKeyColumns(String colName)
+	throws NullPointerException, IllegalArgumentException {
+		if (colName == null) throw new NullPointerException(EX_COL_NULL);
+
+		Column c = getColumn(colName);
+
+		if (c == null) {
+			throw new IllegalArgumentException(EX_FCOL_NE + colName);
+		} else {
+      if(c.multiColumnFK != null){
+        return c.multiColumnFK;
+      }else{
+        String[] ret = new String[1];
+        ret[0] = c.colName;
+        return ret;
+      }
+		}
+	}
+
+ /**
+  * @return a String representation of the Foreign key the given column
+  *         is part of, (fk1, fk2, ...)
+  * */
+   public String getForeignKeyRepresentation(String colName)
+     throws NullPointerException, IllegalArgumentException {
+   if (colName == null) throw new NullPointerException(EX_COL_NULL);
+
+		Column c = getColumn(colName);
+
+		if (c == null) {
+			throw new IllegalArgumentException(EX_FCOL_NE + colName);
+		} else {
+      if(! c.fk){
+        throw new IllegalArgumentException("Column is not part of a foreign key: "+colName);
+      }else{
+        if(c.multiColumnFK == null){
+          return "( "+colName+" )";
+        }else{
+          String str = "( "+c.multiColumnFK[0];
+          for(int i=1; i<c.multiColumnFK.length; i++){
+            str += ", "+c.multiColumnFK[i];
+          }
+          return str+" )";
+        }
+      }
+		}
+	}
+
+	/**
+	 * @return a String representation of all primary key columns,
 	 *         which is the column name in case of single column primary key
 	 *         or "(pk1, pk2, ..., pkn)" in case of a multi column key
 	 */
 	public String getPrimaryKeyRepresentation() {
 		StringBuffer result = new StringBuffer();
 		String pks[] = getPrimaryKeyColumns();
-		
+
 		if (pks.length > 1) result.append("(");
 		for (int i=0; i<pks.length; i++) {
 			if (i>0) result.append(",");
-			result.append(pks[i]);			
+			result.append(pks[i]);
 		}
 		if (pks.length > 1) result.append(")");
-		
+
 		return result.toString();
 	}
-	
+
 	/**
          * @return the name of the table
          */
@@ -424,21 +594,87 @@ public class Table {
 			return true;
 		}
 	}
-        
+
         /**
          * @return a set of attribute names that are associated to the table columns
          */
-        public Set attributes() { 
+        public Set attributes() {
             Set result = new HashSet();
             String att;
-            
+
             for (Iterator i=columns.iterator(); i.hasNext(); ) {
-                att = ((Column)i.next()).attName.trim();
-                if ((att != null) && (!att.equals(""))) result.add(att);
+                att = ((Column)i.next()).attName;
+                if (att != null )
+                  if(!att.trim().equals("")) result.add(att.trim());
             }
-            
+
             return Collections.unmodifiableSet(result);
         }
+  /**
+   * sets the Table's name
+   */
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+    /**
+     * marks a column as optional column (may contain null values)
+     * @exception IlleglArgumentException if column does not exist
+     * */
+  public void setOptional(String colName) throws IllegalArgumentException {
+    Column c = getColumn(colName);
+		if (c == null) {
+      throw new IllegalArgumentException(EX_COL_NE + colName);
+		} else {
+      c.optional = true;
+    }
+  }
+
+    /**
+     * @return true, if the column may contain null-values
+     * @exception IlleglArgumentException if column does not exist
+     * */
+  public boolean isOptional(String colName) throws IllegalArgumentException {
+    Column c = getColumn(colName);
+		if (c == null) {
+      throw new IllegalArgumentException(EX_COL_NE + colName);
+		} else {
+      return c.optional;
+    }
+  }
+
+  /**
+   * @param colName the column to remove
+   * @exception IlleglArgumentException if column is Part of the primary key or of a foreign key
+   **/
+  public void removeColumn(String colName) throws IllegalArgumentException{
+    Column c = getColumn(colName);
+		if (c == null) {
+      return;
+		} else {
+      if(c.pk)
+        throw new IllegalArgumentException(colName +" is part of primary key");
+      if(c.fk)
+        throw new IllegalArgumentException(colName +" is part of foreign key");
+      columns.remove(c);
+    }
+  }
+
+  /**
+   * @exception IlleglArgumentException if primary key is referred to
+   **/
+  public void removePrimaryKey() throws IllegalArgumentException{
+    String[] cols = getPrimaryKeyColumns();
+    for(int i = 0; i<cols.length; i++){
+      Column c = getColumn(cols[i]);
+      if(c.referringTables.size() > 0)
+        throw new IllegalArgumentException(cols[i] +" is referred to");
+      c.pk = false;
+    }
+  }
+
+
 
 	/**
          * @return a string representation of a table object
@@ -497,13 +733,16 @@ public class Table {
 
 		return null;
         }
-        
+
         private class Column {
 		Column(String attName, String type, String colName, boolean pk) {
 			this.colName = colName;
 			this.attName = attName;
 			this.type = type;
 			this.pk = pk;
+      referringTables = new Vector();
+      referringColumns = new Vector();
+      optional = false;
 		}
 
 		String colName;
@@ -511,7 +750,13 @@ public class Table {
 		String type;
 		String fkTable;
 		String fkColumn;
+    Table  realFKTable;
+    List referringTables; //List of Table
+    List referringColumns; //List of String, corresponding referring Table
+                            // to make (Table, Column) pairs referring (thisTable, thisColumn)
+    String[] multiColumnFK; //contains all columns that are part of the same foreign key
 		boolean pk;
 		boolean fk;
+    boolean optional; //true, if column may be null
 	}
 }
