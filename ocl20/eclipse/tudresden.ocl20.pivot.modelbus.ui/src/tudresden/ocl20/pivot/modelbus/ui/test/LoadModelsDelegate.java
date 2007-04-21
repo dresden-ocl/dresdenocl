e485b653d406dcad40824c283b0d5a787d8d341a
mbraeuer <mbraeuer@localhost>
2007-04-21 07:39:04 +0000

    added the action set that tests model loading (moved from modelbus plugin)

---
+++ ocl20/eclipse/tudresden.ocl20.pivot.modelbus.ui/src/tudresden/ocl20/pivot/modelbus/ui/test/LoadModelsDelegate.java
@@ -0,0 +1,82 @@
+package tudresden.ocl20.pivot.modelbus.ui.test;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.actions.ActionDelegate;
+
+import tudresden.ocl20.pivot.modelbus.IMetamodel;
+import tudresden.ocl20.pivot.modelbus.IMetamodelRegistry;
+import tudresden.ocl20.pivot.modelbus.IModel;
+import tudresden.ocl20.pivot.modelbus.IModelProvider;
+import tudresden.ocl20.pivot.modelbus.ModelBusPlugin;
+
+/**
+ * 
+ * 
+ * @author Matthias Braeuer
+ * @version 1.0 05.04.2007
+ */
+public class LoadModelsDelegate extends ActionDelegate implements IWorkbenchWindowActionDelegate {
+
+  /**
+   * Logger for this class
+   */
+  private static final Logger logger = Logger.getLogger(LoadModelsDelegate.class);
+
+  private IWorkbenchWindow window;
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
+   */
+  public void init(IWorkbenchWindow window) {
+    this.window = window;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.ui.actions.ActionDelegate#run(org.eclipse.jface.action.IAction)
+   */
+  @Override
+  public void run(IAction action) {
+    StringBuilder message = new StringBuilder();
+
+    IMetamodelRegistry registry = ModelBusPlugin.getMetamodelRegistry();
+    IMetamodel[] metamodels = registry.getMetamodels();
+
+    IMetamodel ecore = metamodels[0];
+
+    IModelProvider ecoreModelProvider = ecore.getModelProvider();
+
+    IModel pml,pivotmodel;
+
+    try {
+      pml = ecoreModelProvider.getModel(FileLocator.resolve(FileLocator.find(Platform
+          .getBundle("tudresden.ocl20.pivot.examples.pml"),new Path("model/pml.ecore"),null)));
+      pivotmodel = ecoreModelProvider.getModel(FileLocator.resolve(FileLocator.find(Platform
+          .getBundle("tudresden.ocl20.pivot.pivotmodel"),new Path("model/pivotmodel.ecore"),null)));
+      
+      ModelBusPlugin.getModelRegistry().addModel(pml);
+      ModelBusPlugin.getModelRegistry().addModel(pivotmodel);
+      
+      window.getActivePage().showView("tudresden.ocl20.pivot.modelbus.ui.views.models");
+    }
+    
+    catch (Exception e) {
+      MessageDialog.openError(window.getShell(),"Error","Error: " + e.getMessage());
+      logger.error("Error",e);
+      return;
+    }
+
+
+  }
+
+}
