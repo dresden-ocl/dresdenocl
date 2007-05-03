f76f1b7550405b5fe2a4fbf241145dc5486c469a
mbraeuer <mbraeuer@localhost>
2007-05-03 19:17:50 +0000

    added an attribute 'kind' to facilitate type conformance checking of collection types

--- ocl20/eclipse/tudresden.ocl20.pivot.essentialocl/src/tudresden/ocl20/pivot/essentialocl/types/CollectionType.java
+++ ocl20/eclipse/tudresden.ocl20.pivot.essentialocl/src/tudresden/ocl20/pivot/essentialocl/types/CollectionType.java
@@ -32,6 +32,7 @@
  */
 package tudresden.ocl20.pivot.essentialocl.types;
 
+import tudresden.ocl20.pivot.essentialocl.expressions.CollectionKind;
 import java.util.List;
 
 import tudresden.ocl20.pivot.pivotmodel.Type;
@@ -47,6 +48,7 @@ import tudresden.ocl20.pivot.pivotmodel.TypeParameter;
  * <ul>
  *   <li>{@link tudresden.ocl20.pivot.essentialocl.types.CollectionType#getElementType <em>Element Type</em>}</li>
  *   <li>{@link tudresden.ocl20.pivot.essentialocl.types.CollectionType#getOclLibrary <em>Ocl Library</em>}</li>
+ *   <li>{@link tudresden.ocl20.pivot.essentialocl.types.CollectionType#getKind <em>Kind</em>}</li>
  * </ul>
  * </p>
  *
@@ -112,6 +114,44 @@ public interface CollectionType extends Type {
    * @generated
    */
   void setOclLibrary(OclLibrary value);
+
+  /**
+   * Returns the value of the '<em><b>Kind</b></em>' attribute.
+   * The literals are from the enumeration {@link tudresden.ocl20.pivot.essentialocl.expressions.CollectionKind}.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Kind</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * <!-- begin-model-doc -->
+   * <p>
+   * Identifies the kind of this <code>CollectionType</code>. This is necessary
+   * to clearly differentiate an instance of <code>CollectionType</code> from
+   * instances of its subclasses. This is required when determining type conformance
+   * of one collection type with another (see the documentation of {@link #conformsTo}).
+   * Unfortunately, there is no equivalent in Java for the OCL <code>oclIsTypeOf</code>
+   * operation. The <code>instanceof</code> operator cannot be used since it would
+   * return <code>true</code> for subclasses as well.
+   * </p>
+   * <!-- end-model-doc -->
+   * @return the value of the '<em>Kind</em>' attribute.
+   * @see tudresden.ocl20.pivot.essentialocl.expressions.CollectionKind
+   * @see #setKind(CollectionKind)
+   * @generated
+   */
+  CollectionKind getKind();
+
+  /**
+   * Sets the value of the '{@link tudresden.ocl20.pivot.essentialocl.types.CollectionType#getKind <em>Kind</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Kind</em>' attribute.
+   * @see tudresden.ocl20.pivot.essentialocl.expressions.CollectionKind
+   * @see #getKind()
+   * @generated
+   */
+  void setKind(CollectionKind value);
 
   /**
    * Overridden to specialize the co-variant return type to <code>CollectionType</code>.
