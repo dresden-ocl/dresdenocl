c1d3ee15af936874f4a70bead1649ea57c445348
dblcdblu <claaswilke@gmx.net>
2008-05-15 11:42:55 +0000

    Fixed the problem with dependency between tudresden.ocl20.interpreter.ui.InterpreterView.java and tudresden.ocl20.pivot.modelbus.ui.internal.views.ModelsView.java.

--- ocl20/eclipse/tudresden.ocl20.pivot.modelbus/src/tudresden/ocl20/pivot/modelbus/IModelBusConstants.java
+++ ocl20/eclipse/tudresden.ocl20.pivot.modelbus/src/tudresden/ocl20/pivot/modelbus/IModelBusConstants.java
@@ -34,4 +34,15 @@ public interface IModelBusConstants {
    * The name of the attribute containing the name of the metamodel. 
    */
   String ATT_NAME = "name"; //$NON-NLS-1$
+  
+  /**
+   * The ID of the class tudresden.ocl20.pivot.modelbus.ui.views.ModelsView.
+   */
+  public static final String MODELS_VIEW_ID = "tudresden.ocl20.pivot.modelbus.ui.views.models"; //$NON-NLS-1$
+
+  /**
+   * The ID of the class tudresden.ocl20.pivot.modelbus.ui.views.ModelInstancesView.
+   */
+  public static final String MODEL_INSTANCES_VIEW_ID = "tudresden.ocl20.pivot.modelbus.ui.internal.views.ModelInstancesView"; //$NON-NLS-1$
+
 }
