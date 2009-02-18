4a9ca3fb8547b919d3ff4233fd01b162f1b97e51
dblcdblu <claaswilke@gmx.net>
2009-02-18 14:27:04 +0000

    Converted example into a plug-in project.

---
+++ ocl20forEclipse/eclipse/tudresden.ocl20.pivot.examples.simple.constraints/src/tudresden/ocl20/pivot/examples/simple/Professor.java
@@ -0,0 +1,5 @@
+package tudresden.ocl20.pivot.examples.simple;
+
+public class Professor extends Person {
+
+}
---
+++ ocl20forEclipse/eclipse/tudresden.ocl20.pivot.examples.simple.constraints/src/tudresden/ocl20/pivot/examples/simple/Student.java
@@ -0,0 +1,5 @@
+package tudresden.ocl20.pivot.examples.simple;
+
+public class Student extends Person {
+
+}
