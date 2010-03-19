70845d8f98fe12f9bb4050ae360701d4135ec5c6
dblcdblu <claaswilke@gmx.net>
2010-03-19 15:36:52 +0000

    Added test cases for new parser functionality.

--- ocl20forEclipse/eclipse/tudresden.ocl20.pivot.ocl2parser.test/src/tudresden/ocl20/pivot/ocl2parser/test/TestInterface.java
+++ ocl20forEclipse/eclipse/tudresden.ocl20.pivot.ocl2parser.test/src/tudresden/ocl20/pivot/ocl2parser/test/TestInterface.java
@@ -20,8 +20,10 @@ with Dresden OCL2 for Eclipse. If not, see <http://www.gnu.org/licenses/>.
 package tudresden.ocl20.pivot.ocl2parser.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
@@ -286,6 +288,99 @@ public class TestInterface {
 
 		/* The defined operation should not exist. */
 		assertNull(definedOperation);
+	}
+
+	/**
+	 * <p>
+	 * A test case to parse an expression that should be parsed appropriately.
+	 * </p>
+	 */
+	@Test
+	public void testParseConstraintsPositive04_withAdd() throws Exception {
+
+		TestPerformer testPerformer;
+
+		String modelFileName;
+		String oclFileName;
+
+		oclFileName = "interface/constraintsPositive01.ocl";
+		modelFileName = "testmodel.uml";
+
+		/* Try to get the TestPerformer. */
+		testPerformer = TestPerformer.getInstance(
+				AllConstraintTypeTests.META_MODEL_ID,
+				AllConstraintTypeTests.MODEL_BUNDLE,
+				AllConstraintTypeTests.MODEL_DIRECTORY);
+		testPerformer.setModel(modelFileName);
+
+		int existingConstraints;
+		existingConstraints = testPerformer.getCurrentModel().getConstraints()
+				.size();
+
+		ModelListenerMock listener;
+		listener = new ModelListenerMock();
+		testPerformer.getCurrentModel().addListener(listener);
+
+		/* Try to parse the constraint file. */
+		testPerformer.parseFile(oclFileName, true);
+
+		/* The model should contain one newly parsed constraints. */
+		assertEquals(existingConstraints + 1, testPerformer.getCurrentModel()
+				.getConstraints().size());
+
+		/* The listener should have been notified. */
+		assertTrue(listener.wasNotified);
+
+		/* Remove the listener. */
+		testPerformer.getCurrentModel().removeListener(listener);
+
+		/* Remove the constraints. */
+		testPerformer.getCurrentModel().removeAllConstraints();
+	}
+
+	/**
+	 * <p>
+	 * A test case to parse an expression that should be parsed appropriately.
+	 * </p>
+	 */
+	@Test
+	public void testParseConstraintsPositive04_withoutAdd() throws Exception {
+
+		TestPerformer testPerformer;
+
+		String modelFileName;
+		String oclFileName;
+
+		oclFileName = "interface/constraintsPositive01.ocl";
+		modelFileName = "testmodel.uml";
+
+		/* Try to get the TestPerformer. */
+		testPerformer = TestPerformer.getInstance(
+				AllConstraintTypeTests.META_MODEL_ID,
+				AllConstraintTypeTests.MODEL_BUNDLE,
+				AllConstraintTypeTests.MODEL_DIRECTORY);
+		testPerformer.setModel(modelFileName);
+
+		int existingConstraints;
+		existingConstraints = testPerformer.getCurrentModel().getConstraints()
+				.size();
+
+		ModelListenerMock listener;
+		listener = new ModelListenerMock();
+		testPerformer.getCurrentModel().addListener(listener);
+
+		/* Try to parse the constraint file. */
+		testPerformer.parseFile(oclFileName, false);
+
+		/* The model should contain no newly parsed constraints. */
+		assertEquals(existingConstraints, testPerformer.getCurrentModel()
+				.getConstraints().size());
+
+		/* The listener should not have been notified. */
+		assertFalse(listener.wasNotified);
+
+		/* Remove the listener. */
+		testPerformer.getCurrentModel().removeListener(listener);
 	}
 
 	/**
@@ -558,5 +653,107 @@ public class TestInterface {
 			/* The defined operation should not exist. */
 			assertNull(definedOperation);
 		}
+	}
+
+	/**
+	 * <p>
+	 * A test case to parse an expression that should not be parsed
+	 * appropriately.
+	 * </p>
+	 */
+	@Test
+	public void testParseConstraintsNegative04_withAdd() throws Exception {
+
+		TestPerformer testPerformer;
+
+		String modelFileName;
+		String oclFileName;
+
+		oclFileName = "interface/constraintsNegative01.ocl";
+		modelFileName = "testmodel.uml";
+
+		/* Try to get the TestPerformer. */
+		testPerformer = TestPerformer.getInstance(
+				AllConstraintTypeTests.META_MODEL_ID,
+				AllConstraintTypeTests.MODEL_BUNDLE,
+				AllConstraintTypeTests.MODEL_DIRECTORY);
+		testPerformer.setModel(modelFileName);
+
+		int existingConstraints;
+		existingConstraints = testPerformer.getCurrentModel().getConstraints()
+				.size();
+
+		ModelListenerMock listener;
+		listener = new ModelListenerMock();
+		testPerformer.getCurrentModel().addListener(listener);
+
+		/* Try to parse the constraint file. */
+		try {
+			testPerformer.parseFile(oclFileName, true);
+			fail("Expected exception was not thrown.");
+		}
+
+		catch (ParseException e) {
+			/* The model should not contain any newly parsed constraints. */
+			assertEquals(existingConstraints, testPerformer.getCurrentModel()
+					.getConstraints().size());
+
+			/* The listener should not have been notified. */
+			assertFalse(listener.wasNotified);
+		}
+
+		/* Remove the listener. */
+		testPerformer.getCurrentModel().removeListener(listener);
+	}
+
+	/**
+	 * <p>
+	 * A test case to parse an expression that should not be parsed
+	 * appropriately.
+	 * </p>
+	 */
+	@Test
+	public void testParseConstraintsNegative04_withoutAdd() throws Exception {
+
+		TestPerformer testPerformer;
+
+		String modelFileName;
+		String oclFileName;
+
+		oclFileName = "interface/constraintsNegative01.ocl";
+		modelFileName = "testmodel.uml";
+
+		/* Try to get the TestPerformer. */
+		testPerformer = TestPerformer.getInstance(
+				AllConstraintTypeTests.META_MODEL_ID,
+				AllConstraintTypeTests.MODEL_BUNDLE,
+				AllConstraintTypeTests.MODEL_DIRECTORY);
+		testPerformer.setModel(modelFileName);
+
+		int existingConstraints;
+		existingConstraints = testPerformer.getCurrentModel().getConstraints()
+				.size();
+
+		ModelListenerMock listener;
+		listener = new ModelListenerMock();
+		testPerformer.getCurrentModel().addListener(listener);
+
+		/* Try to parse the constraint file. */
+		try {
+			testPerformer.parseFile(oclFileName, false);
+			fail("Expected exception was not thrown.");
+		}
+
+		catch (ParseException e) {
+			/* The model should not contain any newly parsed constraints. */
+			assertEquals(existingConstraints, testPerformer.getCurrentModel()
+					.getConstraints().size());
+
+			/* The listener should not have been notified. */
+			assertFalse(listener.wasNotified);
+		}
+
+		/* Remove the listener. */
+		testPerformer.getCurrentModel().removeListener(listener);
 	}
 }
