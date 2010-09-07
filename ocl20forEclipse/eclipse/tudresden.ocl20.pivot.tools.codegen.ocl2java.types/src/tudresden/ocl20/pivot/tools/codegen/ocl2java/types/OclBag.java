243c939e21b11a28b8b1d5288751e676697839fe
dblcdblu <claaswilke@gmx.net>
2010-09-07 13:57:55 +0000

    Added OclInvalidException marked old collection types as deprecated.

--- ocl20forEclipse/eclipse/tudresden.ocl20.pivot.tools.codegen.ocl2java.types/src/tudresden/ocl20/pivot/tools/codegen/ocl2java/types/OclBag.java
+++ ocl20forEclipse/eclipse/tudresden.ocl20.pivot.tools.codegen.ocl2java.types/src/tudresden/ocl20/pivot/tools/codegen/ocl2java/types/OclBag.java
@@ -1,25 +1,27 @@
 /*
-Copyright (C) 2008-2009 by Claas Wilke (claaswilke@gmx.net)
+Copyright (C) 2008-2010 by Claas Wilke (claas.wilke@tu-dresden.de)
 
-This file is part of the OCL 2 Java Code Generator of Dresden OCL2 for Eclipse.
+This file is part of the OCL 2 Java Code Generator of Dresden OCL.
 
-Dresden OCL2 for Eclipse is free software: you can redistribute it and/or modify 
+Dresden OCL is free software: you can redistribute it and/or modify 
 it under the terms of the GNU Lesser General Public License as published by the 
 Free Software Foundation, either version 3 of the License, or (at your option)
 any later version.
 
-Dresden OCL2 for Eclipse is distributed in the hope that it will be useful,
+Dresden OCL is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
 or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
 for more details.
 
 You should have received a copy of the GNU Lesser General Public License along 
-with Dresden OCL2 for Eclipse. If not, see <http://www.gnu.org/licenses/>.
-*/
+with Dresden OCL. If not, see <http://www.gnu.org/licenses/>.
+ */
 package tudresden.ocl20.pivot.tools.codegen.ocl2java.types;
 
 import java.util.ArrayList;
 import java.util.Collection;
+
+import tudresden.ocl20.pivot.tools.codegen.ocl2java.types.util.OclBags;
 
 /**
  * <p>
@@ -32,6 +34,7 @@ import java.util.Collection;
  *            The type of the elements contained in this {@link OclBag}.
  * 
  * @author Claas Wilke
+ * @deprecated Use {@link OclBags} instead.
  */
 public class OclBag<T> extends OclCollection<T> {
 
--- ocl20forEclipse/eclipse/tudresden.ocl20.pivot.tools.codegen.ocl2java.types/src/tudresden/ocl20/pivot/tools/codegen/ocl2java/types/OclOrderedSet.java
+++ ocl20forEclipse/eclipse/tudresden.ocl20.pivot.tools.codegen.ocl2java.types/src/tudresden/ocl20/pivot/tools/codegen/ocl2java/types/OclOrderedSet.java
@@ -1,20 +1,20 @@
 /*
-Copyright (C) 2008-2009 by Claas Wilke (claaswilke@gmx.net)
+Copyright (C) 2008-2010 by Claas Wilke (claas.wilke@tu-dresden.de)
 
-This file is part of the OCL 2 Java Code Generator of Dresden OCL2 for Eclipse.
+This file is part of the OCL 2 Java Code Generator of Dresden OCL.
 
-Dresden OCL2 for Eclipse is free software: you can redistribute it and/or modify 
+Dresden OCL is free software: you can redistribute it and/or modify 
 it under the terms of the GNU Lesser General Public License as published by the 
 Free Software Foundation, either version 3 of the License, or (at your option)
 any later version.
 
-Dresden OCL2 for Eclipse is distributed in the hope that it will be useful,
+Dresden OCL is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
 or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
 for more details.
 
 You should have received a copy of the GNU Lesser General Public License along 
-with Dresden OCL2 for Eclipse. If not, see <http://www.gnu.org/licenses/>.
+with Dresden OCL. If not, see <http://www.gnu.org/licenses/>.
  */
 package tudresden.ocl20.pivot.tools.codegen.ocl2java.types;
 
@@ -22,6 +22,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+
+import tudresden.ocl20.pivot.tools.codegen.ocl2java.types.util.OclOrderedSets;
 
 /**
  * <p>
@@ -33,6 +35,7 @@ import java.util.Set;
  *            The type of the elements contained in this {@link OclOrderedSet}.
  * 
  * @author Claas Wilke
+ * @deprecated Use {@link OclOrderedSets} instead.
  */
 public class OclOrderedSet<T> extends OclCollection<T> implements Set<T> {
 
