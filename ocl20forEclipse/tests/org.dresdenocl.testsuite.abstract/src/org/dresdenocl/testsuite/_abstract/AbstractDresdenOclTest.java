f8b6e0136d45798040312cdedb0ba84fc2bfa235
dblcdblu <claaswilke@gmx.net>
2011-03-26 10:25:07 +0000

    Intitial check-in of abstract and concrete test plug-in to combine all test suites for headless executable tests.

---
+++ ocl20forEclipse/tests/org.dresdenocl.testsuite.abstract/src/org/dresdenocl/testsuite/_abstract/AbstractDresdenOclTest.java
@@ -0,0 +1,314 @@
+/*
+ * Copyright (C) 2011 by Claas Wilke (claas.wilke@tu-dresden.de) This file is part of
+ * Dresden OCL. Dresden OCL is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version. Dresden OCL is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details. You should have received a copy of
+ * the GNU Lesser General Public License along with Dresden OCL. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dresdenocl.testsuite._abstract;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.eclipse.uml2.uml.UMLPackage;
+import org.eclipse.uml2.uml.internal.resource.UMLResourceFactoryImpl;
+import org.eclipse.uml2.uml.resource.UMLResource;
+import org.junit.BeforeClass;
+
+import tudresden.ocl20.logging.LoggingPlugin;
+import tudresden.ocl20.pivot.essentialocl.types.impl.TypesPackageImpl;
+import tudresden.ocl20.pivot.metamodels.ecore.EcoreMetamodelPlugin;
+import tudresden.ocl20.pivot.metamodels.ecore.internal.provider.EcoreModelProvider;
+import tudresden.ocl20.pivot.metamodels.java.JavaMetaModelPlugin;
+import tudresden.ocl20.pivot.metamodels.java.internal.provider.JavaModelProvider;
+import tudresden.ocl20.pivot.metamodels.uml2.UML2MetamodelPlugin;
+import tudresden.ocl20.pivot.metamodels.uml2.internal.provider.UML2ModelProvider;
+import tudresden.ocl20.pivot.model.IModelProvider;
+import tudresden.ocl20.pivot.model.metamodel.IMetamodel;
+import tudresden.ocl20.pivot.model.metamodel.IMetamodelRegistry;
+import tudresden.ocl20.pivot.modelbus.ModelBusPlugin;
+
+/**
+ * Abstract class for Dresden OCL test cases. Contains static setUp and tearDown
+ * methods that initialize Dresden OCL when the Eclipse platform is not running
+ * (headless).
+ * 
+ * @author Claas Wilke
+ */
+@SuppressWarnings("restriction")
+public class AbstractDresdenOclTest {
+
+	/** Whether or not logging has been enabled yet. */
+	private static boolean loggingEnabled = false;
+
+	@BeforeClass
+	public static void setUp() throws IOException {
+
+		/* Initializes Dresden OCL when tests were started headless. */
+		if (!Platform.isRunning()) {
+			initializeLogging();
+			registerEmfResourceFactories();
+			registerMetamodels();
+		}
+		// no else.
+	}
+
+	/** Initializes logging. */
+	protected static void initializeLogging() throws IOException {
+		if (!loggingEnabled) {
+			File loggerPropertiesUrl = getFile("log4j.properties",
+					"org.dresdenocl.testsuite.abstract");
+			new LoggingPlugin(loggerPropertiesUrl.toURI().toURL());
+			loggingEnabled = true;
+		}
+		// no else.
+	}
+
+	/**
+	 * Registers EMF resource factories at EMF.
+	 * 
+	 * @throws IOException
+	 */
+	protected static void registerEmfResourceFactories() throws IOException {
+
+		/* Probably register the Ecore resource for EMF. */
+		if (Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().get(
+				"ecore") == null) {
+
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+					"ecore", new XMIResourceFactoryImpl() {
+						public Resource createResource(URI uri) {
+							XMIResource xmiResource = new XMIResourceImpl(uri);
+							return xmiResource;
+						}
+					});
+		}
+		// no else.
+
+		if (EPackage.Registry.INSTANCE.getEPackage(EcorePackage.eNS_URI) == null) {
+			EPackage.Registry.INSTANCE.put(EcorePackage.eNS_PREFIX,
+					EcorePackage.eINSTANCE);
+		}
+		// no else.
+
+		/* Probably register the UML resource for EMF. */
+		if (Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().get(
+				"uml") == null) {
+
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+					"uml", new XMIResourceFactoryImpl() {
+						public Resource createResource(URI uri) {
+							XMIResource xmiResource = new XMIResourceImpl(uri);
+							return xmiResource;
+						}
+					});
+		}
+		// no else.
+
+		if (EPackage.Registry.INSTANCE.getEPackage(UMLPackage.eNS_URI) == null) {
+			EPackage.Registry.INSTANCE.put(UMLPackage.eNS_PREFIX,
+					UMLPackage.eINSTANCE);
+			loadUmlResources();
+		}
+		// no else.
+
+		/* Probably register the types resource for EMF. */
+		if (Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().get(
+				"types") == null) {
+
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+					"types", new XMIResourceFactoryImpl() {
+						public Resource createResource(URI uri) {
+							XMIResource xmiResource = new XMIResourceImpl(uri);
+							return xmiResource;
+						}
+					});
+		}
+		// no else.
+
+		if (EPackage.Registry.INSTANCE.getEPackage(TypesPackageImpl.eNS_URI) == null) {
+			EPackage.Registry.INSTANCE.put(TypesPackageImpl.eNS_PREFIX,
+					TypesPackageImpl.eINSTANCE);
+		}
+		// no else.
+	}
+
+	/**
+	 * Loads resources required for the UML meta model.
+	 * 
+	 * @throws IOException
+	 */
+	protected static void loadUmlResources() throws IOException {
+
+		File umlResources = getFile(
+				"lib/org.eclipse.uml2.uml.resources_3.1.1.v201008191505.jar",
+				"org.dresdenocl.testsuite.abstract");
+		if (umlResources == null)
+			throw new IllegalArgumentException(
+					"Cannot laod an UML model with umlResources == null; umlResources has to point to the jar file of the plugin org.eclipse.uml2.uml.resources.");
+
+		EPackage.Registry.INSTANCE
+				.put(UMLPackage.eNS_URI, UMLPackage.eINSTANCE);
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				UMLResource.FILE_EXTENSION, UMLResourceFactoryImpl.INSTANCE);
+
+		URI pluginURI = URI.createURI("jar:file:"
+				+ umlResources.getAbsolutePath() + "!/");
+		URIConverter.URI_MAP.put(URI.createURI(UMLResource.LIBRARIES_PATHMAP),
+				pluginURI.appendSegment("libraries").appendSegment(""));
+		URIConverter.URI_MAP.put(URI.createURI(UMLResource.METAMODELS_PATHMAP),
+				pluginURI.appendSegment("metamodels").appendSegment(""));
+		URIConverter.URI_MAP.put(URI.createURI(UMLResource.PROFILES_PATHMAP),
+				pluginURI.appendSegment("profiles").appendSegment(""));
+
+	}
+
+	/** Registers {@link IMetamodel}s at the {@link IMetamodelRegistry}. */
+	protected static void registerMetamodels() {
+
+		/* Probably register the Ecore metamodel. */
+		IMetamodel ecoreMetamodel = ModelBusPlugin.getMetamodelRegistry()
+				.getMetamodel(EcoreMetamodelPlugin.ID);
+		if (ecoreMetamodel == null) {
+			ecoreMetamodel = new IMetamodel() {
+
+				IModelProvider provider = new EcoreModelProvider();
+
+				public String getName() {
+					return EcoreMetamodelPlugin.ID;
+				}
+
+				public IModelProvider getModelProvider() {
+					return provider;
+				}
+
+				public String getId() {
+					return EcoreMetamodelPlugin.ID;
+				}
+			};
+			ModelBusPlugin.getMetamodelRegistry().addMetamodel(ecoreMetamodel);
+		}
+
+		/* Probably register the Java metamodel. */
+		IMetamodel javaMetamodel = ModelBusPlugin.getMetamodelRegistry()
+				.getMetamodel(JavaMetaModelPlugin.ID);
+		if (javaMetamodel == null) {
+			javaMetamodel = new IMetamodel() {
+
+				IModelProvider provider = new JavaModelProvider();
+
+				public String getName() {
+					return JavaMetaModelPlugin.ID;
+				}
+
+				public IModelProvider getModelProvider() {
+					return provider;
+				}
+
+				public String getId() {
+					return JavaMetaModelPlugin.ID;
+				}
+			};
+			ModelBusPlugin.getMetamodelRegistry().addMetamodel(javaMetamodel);
+		}
+
+		/* Probably register the UML metamodel. */
+		IMetamodel umlMetamodel = ModelBusPlugin.getMetamodelRegistry()
+				.getMetamodel(UML2MetamodelPlugin.ID);
+		if (umlMetamodel == null) {
+			umlMetamodel = new IMetamodel() {
+
+				IModelProvider provider = new UML2ModelProvider();
+
+				public String getName() {
+					return UML2MetamodelPlugin.ID;
+				}
+
+				public IModelProvider getModelProvider() {
+					return provider;
+				}
+
+				public String getId() {
+					return UML2MetamodelPlugin.ID;
+				}
+			};
+			ModelBusPlugin.getMetamodelRegistry().addMetamodel(umlMetamodel);
+		}
+	}
+
+	/**
+	 * <p>
+	 * Returns the file object for a given path relative to the plug-in's
+	 * directory.
+	 * </p>
+	 * 
+	 * @param path
+	 *            The path of the resource.
+	 * @param bundleId
+	 *            The ID of the Bundle containing the resource.
+	 * @return The found {@link File} object.
+	 * @throws IOException
+	 * @throws Exception
+	 *             Thrown, if the opening fails.
+	 */
+	public static File getFile(String path, String bundleId) throws IOException {
+
+		URL fileLocation;
+		File file;
+
+		if (Platform.isRunning()) {
+			fileLocation = Platform.getBundle(bundleId).getResource(path);
+			fileLocation = FileLocator.resolve(fileLocation);
+			file = new File(fileLocation.getFile());
+		}
+
+		else {
+			File currentLocation = new File("./");
+			File bundleFile = null;
+
+			while (currentLocation != null && currentLocation.exists()
+					&& currentLocation.isDirectory()) {
+				bundleFile = new File(currentLocation.getAbsolutePath()
+						+ File.separator + bundleId);
+
+				if (bundleFile.exists() && bundleFile.isDirectory())
+					break;
+				else {
+					bundleFile = null;
+					currentLocation = new File(
+							currentLocation.getAbsolutePath() + File.separator
+									+ ".." + File.separator);
+				}
+			}
+
+			if (bundleFile != null)
+				file = new File(bundleFile + File.separator + path);
+
+			else
+				throw new RuntimeException("Bundle or directory '" + bundleId
+						+ "' was not found.");
+		}
+
+		assertTrue(file.exists());
+
+		return file;
+	}
+}
---
+++ ocl20forEclipse/tests/org.dresdenocl.testsuite/src/org/dresdenocl/testsuite/DresdenOclTestSuite.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2011 by Claas Wilke (claas.wilke@tu-dresden.de) This file is part of
+ * Dresden OCL. Dresden OCL is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version. Dresden OCL is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details. You should have received a copy of
+ * the GNU Lesser General Public License along with Dresden OCL. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dresdenocl.testsuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import tudresden.ocl20.pivot.metamodels.ecore.test.tests.AllEcoreMetaModelTests;
+import tudresden.ocl20.pivot.metamodels.java.test.tests.TestJavaMetaModel;
+import tudresden.ocl20.pivot.metamodels.uml2.test.tests.AllUml2MetaModelTests;
+import tudresden.ocl20.pivot.metamodels.xsd.test.AllXmlSchemaMetamodelTests;
+
+/**
+ * <p>
+ * Collects all test suites of Dresden OCL.
+ * </p>
+ * 
+ * @author Claas Wilke
+ */
+@RunWith(Suite.class)
+// FIXME Claas: Collect all test suites.
+@Suite.SuiteClasses({ AllEcoreMetaModelTests.class,
+		AllUml2MetaModelTests.class, /* AllXmlSchemaMetamodelTests.class, */
+		TestJavaMetaModel.class })
+public class DresdenOclTestSuite {
+	/*
+	 * This class remains completely empty, being used only as a holder for the
+	 * above annotations.
+	 */
+}
