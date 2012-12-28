2148ce722a045e44046f52ef7ce27382e3ad1ff1
Lars Schuetze <Lars.Schuetze@mailbox.tu-dresden.de>
2012-12-28 16:37:47 +0100

    tracer does now support some commands

---
+++ eclipse/org.dresdenocl.tracer.ui/src/org/dresdenocl/tracer/ui/command/handler/TracerViewCommandHandler.java
@@ -0,0 +1,26 @@
+package org.dresdenocl.tracer.ui.command.handler;
+
+import org.dresdenocl.tracer.ui.TracerUIPlugin;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+public class TracerViewCommandHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		try {
+			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+					.showView(TracerUIPlugin.TRACER_VIEW_ID);
+		}
+
+		catch (PartInitException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
