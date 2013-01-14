b02f9e300e7c0e52c3c57985781781eeac0ee5fc
Lars Schuetze <Lars.Schuetze@mailbox.tu-dresden.de>
2013-01-14 13:11:15 +0100

    Implemented GUI Launch support

--- org.dresdenocl.debug.ui/src/org/dresdenocl/debug/ui/OclDebugUIPlugin.java
+++ org.dresdenocl.debug.ui/src/org/dresdenocl/debug/ui/OclDebugUIPlugin.java
@@ -6,44 +6,57 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class DebugUIPlugin extends AbstractUIPlugin {
+public class OclDebugUIPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.dresdenocl.debug.ui"; //$NON-NLS-1$
 
-	// The shared instance
-	private static DebugUIPlugin plugin;
+	public static final String LAUNCH_CONFIGURATION_TYPE =
+			"org.dresdenocl.debug.launch.OclLaunchConfigurationDelegate"; //$NON-NLS-1$
 	
+	public static final String OCL_URI = "uri";
+
+	// The shared instance
+	private static OclDebugUIPlugin plugin;
+
 	/**
 	 * The constructor
 	 */
-	public DebugUIPlugin() {
+	public OclDebugUIPlugin() {
+
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
+
 		super.start(context);
 		plugin = this;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
+
 		plugin = null;
 		super.stop(context);
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
-	public static DebugUIPlugin getDefault() {
+	public static OclDebugUIPlugin getDefault() {
+
 		return plugin;
 	}
 
